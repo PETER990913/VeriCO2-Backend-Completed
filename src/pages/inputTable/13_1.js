8f0cd91e63e6c15f5d7bfb309c467f6d42ed47b7
PETER990913 <jeremy.t.1132@gmail.com>
2023-09-22 06:54:50 -0700

    'update1'

--- src/pages/inputTable/13_1.js
+++ src/pages/inputTable/13_1.js
@@ -11,11 +11,20 @@ function FranchisesSpecific({onChange, dataset, dataset2}) {
     const [v7, setV7]= useState(30000);
     const [v8, setV8]= useState(30000);
     const [v9, setV9]= useState(10000);
+    const [v30, setV30]= useState(40000);
+    const [v31, setV31]= useState(10000);
+    const [v32, setV32]= useState(60000);
+    const [v33, setV33]= useState(20000);
+    const [v34, setV34]= useState(10000);
+    const [v35, setV35]= useState(90000);
     const [v10, setV10]= useState('1');
     const [v11, setV11]= useState('2');
     const [v12, setV12]= useState('3');
     const [v13, setV13]= useState('4');
     const [v14, setV14]= useState('5');
+    const [v15, setV15]= useState('6');
+    const [v16, setV16]= useState('7');
+    const [v17, setV17]= useState('8');
 
     useEffect(() => {
         console.log('dataset', dataset)
@@ -78,10 +87,37 @@ function FranchisesSpecific({onChange, dataset, dataset2}) {
         if(v8) setV8(v8)
         if(v9) setV9(v9)
     }
+    const handleOnChange5 = (e) => {
+        console.log('handle change->', e.target.value);
+        const keyVal = e.target.value
+        setV15(e.target.value)
+        const v30 = dataset2[keyVal] ? dataset2[keyVal][0] : 0
+        const v31 = dataset2[keyVal] ? dataset2[keyVal][1] : 0
+        if(v30) setV30(v30)
+        if(v31) setV31(v31)
+    }
+    const handleOnChange6 = (e) => {
+        console.log('handle change->', e.target.value);
+        const keyVal = e.target.value
+        setV16(e.target.value)
+        const v32 = dataset2[keyVal] ? dataset2[keyVal][0] : 0
+        const v33 = dataset2[keyVal] ? dataset2[keyVal][1] : 0
+        if(v32) setV32(v32)
+        if(v33) setV33(v33)
+    }
+    const handleOnChange7 = (e) => {
+        console.log('handle change->', e.target.value);
+        const keyVal = e.target.value
+        setV17(e.target.value)
+        const v34 = dataset2[keyVal] ? dataset2[keyVal][0] : 0
+        const v35 = dataset2[keyVal] ? dataset2[keyVal][1] : 0
+        if(v35) setV35(v35)
+        if(v34) setV34(v34)
+    }
     useEffect(()=>{
-        onChange(Number(v0)+Number(v1)+Number(v2)+Number(v3)+Number(v4)+Number(v5)+Number(v6)+Number(v7)+Number(v8)+Number(v9))
+        onChange(Number(v0)+Number(v1)+Number(v2)+Number(v3)+Number(v4)+Number(v5)+Number(v6)+Number(v7)+Number(v8)+Number(v9)+Number(v30)+Number(v31)+Number(v32)+Number(v33)+Number(v34)+Number(v35))
     },[
-        v0,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10
+        v0,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v30,v31,v32,v33,v34,v35
     ])
     return (
         // <div className='SignupPage' onClick={() => setfake1(true)}>
@@ -119,6 +155,21 @@ function FranchisesSpecific({onChange, dataset, dataset2}) {
                             <td><input type='text' value={v14} className='Input_form' onChange={handleOnChange4}/></td>
                             <td><input type='text' value={v8} className='Input_form' onChange={(e)=>{setV8(e.target.value)}}/></td>
                             <td><input type='text' value={v9} className='Input_form' onChange={(e)=>{setV9(e.target.value)}}/></td>
+                        </tr>
+                        <tr>
+                            <td><input type='text' value={v15} className='Input_form' onChange={handleOnChange5}/></td>
+                            <td><input type='text' value={v30} className='Input_form' onChange={(e)=>{setV30(e.target.value)}}/></td>
+                            <td><input type='text' value={v31} className='Input_form' onChange={(e)=>{setV31(e.target.value)}}/></td>
+                        </tr>
+                        <tr>
+                            <td><input type='text' value={v16} className='Input_form' onChange={handleOnChange6}/></td>
+                            <td><input type='text' value={v32} className='Input_form' onChange={(e)=>{setV32(e.target.value)}}/></td>
+                            <td><input type='text' value={v33} className='Input_form' onChange={(e)=>{setV33(e.target.value)}}/></td>
+                        </tr>
+                        <tr>
+                            <td><input type='text' value={v17} className='Input_form' onChange={handleOnChange7}/></td>
+                            <td><input type='text' value={v34} className='Input_form' onChange={(e)=>{setV34(e.target.value)}}/></td>
+                            <td><input type='text' value={v35} className='Input_form' onChange={(e)=>{setV35(e.target.value)}}/></td>
                         </tr>
                     </tbody>
                 </table>
